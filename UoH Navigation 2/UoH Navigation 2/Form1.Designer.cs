fcb2e15c5ecf756a7232a7083f92438b61f3e5c2
JKGelder <78348536+JKGelder@users.noreply.github.com>
2021-12-09 14:07:02 +0000

    Add files via upload
    The project so far:
    Main page to chose building and room
    second page pops up and shows directions

---
+++ UoH Navigation 2/UoH Navigation 2/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace UoH_Navigation_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.selection1 = new System.Windows.Forms.ComboBox();
+            this.selection2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(395, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Directions";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // selection1
+            // 
+            this.selection1.FormattingEnabled = true;
+            this.selection1.Location = new System.Drawing.Point(221, 164);
+            this.selection1.Name = "selection1";
+            this.selection1.Size = new System.Drawing.Size(168, 21);
+            this.selection1.TabIndex = 1;
+            this.selection1.SelectedIndexChanged += new System.EventHandler(this.selection_SelectedIndexChanged);
+            // 
+            // selection2
+            // 
+            this.selection2.FormattingEnabled = true;
+            this.selection2.Location = new System.Drawing.Point(478, 164);
+            this.selection2.Name = "selection2";
+            this.selection2.Size = new System.Drawing.Size(168, 21);
+            this.selection2.TabIndex = 2;
+            this.selection2.SelectedIndexChanged += new System.EventHandler(this.selection2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose the building you want to go to";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(475, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Choose the room you want to go to";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = global::UoH_Navigation_2.Properties.Resources.UoH_new__logo_black__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(60, 234);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(690, 346);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.selection2);
+            this.Controls.Add(this.selection1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox selection1;
+        private System.Windows.Forms.ComboBox selection2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ UoH Navigation 2/UoH Navigation 2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace UoH_Navigation_2
+{
+    public partial class Form1 : Form
+    {
+        List<string> building;
+        List<string> room;
+        List<string> directions;
+        List<string> whereFrom;
+
+        public Form1()
+        {
+            InitializeComponent();
+            StreamReader sr = new StreamReader("roomDirections.csv");
+            building = new List<string>();
+            room = new List<string>();
+            directions = new List<string>();
+            whereFrom = new List<string>();
+
+            string line;
+            while((line = sr.ReadLine()) != null)
+            {
+                string[] columns = line.Split(',');
+                building.Add(columns[0]);
+                room.Add(columns[1]);
+                directions.Add(columns[2]);
+                whereFrom.Add(columns[3]);
+            }
+            building.RemoveAt(0);
+
+            selection1.Items.AddRange(building.Distinct().ToArray());
+
+            foreach (string place in room)
+            { 
+                selection2.Items.Add(place);
+            }
+        }
+
+
+        
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selection_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selection2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int choice = selection2.SelectedIndex;
+            MessageBox.Show(directions[choice]);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
